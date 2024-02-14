c2bf7bfeb033f5379c400c9688af9a6c6a3972b1
JFVela <jfvela_2012@hotmail.com>
2024-02-13 21:32:31 -0500

    Correción de errores
    se corrijio el error de sumar la cantidad de migras ademas de correcion de algunas validaciones.

--- docs/script.js
+++ docs/script.js
@@ -5,9 +5,12 @@ function calcularTotal() {
 
     ventas.forEach(function(venta) {
         var cantidad = parseInt(venta.getAttribute('data-cantidad')) || 0;
-        totalVentas += cantidad;
+        
+        // Verificar si la venta es de migración
         if (venta.innerText.includes("Migra")) {
-            totalMigras++;
+            totalMigras += cantidad; // Agregar la cantidad de migras al total de migras
+        } else {
+            totalVentas += cantidad; // Agregar la cantidad de ventas al total de ventas
         }
     });
 
@@ -24,6 +27,8 @@ function calcularTotal() {
     document.getElementById('totalVentas').innerText = resumenText;
 }
 
+
+
 function agregarVenta() {
     var cantidad = parseInt(document.getElementById('cantidad').value);
     var nombre = document.getElementById('nombre').value;
@@ -31,7 +36,6 @@ function agregarVenta() {
     var selectElement = document.getElementById('producto');
     var selectedIndex = selectElement.selectedIndex;
     var selectedOptionText = selectElement.options[selectedIndex].text;
-    console.log(selectedOptionText);
     
     // Verificar si se ha ingresado un nombre
     if (nombre.trim() === '') {
@@ -47,17 +51,20 @@ function agregarVenta() {
         if (cantidad === 0) {
             venta.innerText = nombre + ': 0 ❌';
         } else {
-            venta.innerText = nombre + ': ' + cantidad + ' ' + selectedOptionText + ' para Instalación Urgente 💪✅';
-            venta.setAttribute('data-cantidad', cantidad);
-            venta.classList.add('venta');
-
             if (producto === "7") {
+                // Venta de Inducción
                 venta.innerText = nombre + ' (Inducción)';
                 venta.setAttribute('data-cantidad', ''); // No se requiere cantidad
                 venta.classList.add('venta');
             } else if (producto === "6") {
+                // Venta de Migración
                 venta.innerText = nombre + ': ' + cantidad + ' Migra para Instalación Urgente 💪✅';
-                venta.setAttribute('data-cantidad', ''); // No se requiere cantidad
+                venta.setAttribute('data-cantidad', cantidad); // Se agrega la cantidad como atributo
+                venta.classList.add('venta');
+            } else {
+                // Otras ventas
+                venta.innerText = nombre + ': ' + cantidad + ' ' + selectedOptionText + ' para Instalación Urgente 💪✅';
+                venta.setAttribute('data-cantidad', cantidad); // Se agrega la cantidad como atributo
                 venta.classList.add('venta');
             }
         }
